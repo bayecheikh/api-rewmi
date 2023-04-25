152621546e0304489bb9e8b287e00c2faebefd65
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-04-25 23:50:10 +0000

    First commit

--- app/Http/Controllers/Api/RechercheParrainageController.php
+++ app/Http/Controllers/Api/RechercheParrainageController.php
@@ -66,7 +66,7 @@ class RechercheParrainageController extends Controller
 
             if($numero_cedeao!=''){               
                 $Parrainages = $Parrainages
-                ->where('numero_cedeao','like', "%".$numero_cedeao."%");                  
+                ->where('numero_cedeao','like', "%{$input['numero_cedeao']}%");                  
             }
             if($prenom!=''){               
                 $Parrainages = $Parrainages
