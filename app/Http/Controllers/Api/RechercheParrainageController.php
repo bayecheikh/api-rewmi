b4eebcada69ef335358cb058af2e813724df1137
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-04-25 01:06:52 +0000

    First commit

--- app/Http/Controllers/Api/RechercheParrainageController.php
+++ app/Http/Controllers/Api/RechercheParrainageController.php
@@ -56,7 +56,7 @@ class RechercheParrainageController extends Controller
             ->json($validator->errors());
         }
         else{ 
-            $Parrainages = Parrainage::where('status', 'like', '%actif%')->get();
+            $Parrainages = Parrainage::all();
            
             if (!$request->user()->hasRole('super_admin')) {
                 $user_id = $request->user()->id;
