2a013106b5ac67433cd53a59ea7a2dea931844e8
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-09-13 12:35:54 +0000

    update

--- app/Http/Controllers/Api/RechercheParrainageController.php
+++ app/Http/Controllers/Api/RechercheParrainageController.php
@@ -235,7 +235,7 @@ class RechercheParrainageController extends Controller
                 ->select('*')
                 ->orderBy('numero_cedeao', 'desc')
                 ->groupBy('numero_cedeao')
-                ->havingRaw('COUNT(id) > 1')
+                ->havingRaw('COUNT(id) > 2')
                 ->get();
        
         return response()->json(["success" => true, "message" => "Parrainage List en doublon", "data" =>$Parrainages]);
