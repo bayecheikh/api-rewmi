93ca46a37e1997a638f8cdb4cb1781a45103484c
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-10-02 17:07:02 +0000

    update

--- database/migrations/2023_10_02_162941_create_electeurs_table.php
+++ database/migrations/2023_10_02_162941_create_electeurs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('Electeurs', function (Blueprint $table) {
+        Schema::create('electeurs', function (Blueprint $table) {
             $table->id();
             $table->string('numero_cedeao')->nullable();
             $table->string('prenom')->nullable();
