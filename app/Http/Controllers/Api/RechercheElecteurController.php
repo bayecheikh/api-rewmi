da569bb3ba0e149a882257420d7217a3b8045245
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-12-09 11:40:09 +0000

    Update

--- app/Http/Controllers/Api/RechercheElecteurController.php
+++ app/Http/Controllers/Api/RechercheElecteurController.php
@@ -162,7 +162,7 @@ class RechercheElecteurController extends Controller
                 $Electeurs = $Electeurs->take(intval($input['nombre']));                  
             }
             if(isset($input['skip'])){               
-                $Electeurs = $Electeurs->skip(intval($input['nombre']));                  
+                $Electeurs = $Electeurs->skip(intval($input['skip']));                  
             }
 
             return response()->json(["success" => true, "message" => "Liste des Electeurs", "data" =>$Electeurs,"REGION" =>$region]);
