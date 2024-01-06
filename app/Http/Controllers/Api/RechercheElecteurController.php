63608f94040b572d84fd150fa8bf959d421410e9
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2024-01-06 12:05:48 +0000

    Update

--- app/Http/Controllers/Api/RechercheElecteurController.php
+++ app/Http/Controllers/Api/RechercheElecteurController.php
@@ -142,7 +142,7 @@ class RechercheElecteurController extends Controller
                 $Electeurs = $Electeurs
                 ->where('created_at','like', '%'.$created_at.'%');                  
             }
-            if($input['recent_date']){  
+            if(isset($input['recent_date'])){  
                 $recent_date = $input['recent_date'];           
                 $Electeurs = $Electeurs
                 ->where('created_at','>=', $recent_date);                  
